b458cb02f31d329c493c94ac361d1339ea2e3e39
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2021-12-11 20:09:58 +0300

    .

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,6 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ Storage::url('logo.png') }}" class="logo" alt="">
-@else
 {{ $slot }}
 @endif
 </a>
