339bd1161a83781160023042ed7f16baa253b5de
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2021-12-21 18:58:23 +0300

    .

--- app/Http/Livewire/Admin/Deliveries/Edit.php
+++ app/Http/Livewire/Admin/Deliveries/Edit.php
@@ -12,9 +12,9 @@ class Edit extends BaseCrud
         $this->deliveryId = $delivery['id'];
         $this->name = $delivery['name'];
         $this->description = $delivery['description'];
-        $this->price = $delivery['price'];
+        $this->price = $delivery['price']/100;
         $this->active_free_price = $delivery['active_free_price'];
-        $this->free_price = $delivery['free_price'];
+        $this->free_price = $delivery['free_price']/100;
     }
 
     public function update(): void
