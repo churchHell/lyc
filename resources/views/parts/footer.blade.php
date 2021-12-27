ed158220aaa247ffbcb560fb1b241be71e360e85
Your Name <you@example.com>
2021-12-27 14:10:59 +0300

    .

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -3,17 +3,22 @@
         <div class="">
             {!! $settings->get('about')->value !!}
         </div>
-        <div class="flex flex-col">
-            <div>{{ __('contacts') }}</div>
-            <div class="">{{ renderPhone($settings->get('phone1')->value) }}</div>
-            <div class="">{{ renderPhone($settings->get('phone2')->value) }}</div>
-            <div class="">{{ $settings->get('address1')->value }}</div>
+
+        <div class='flex flex-col space-y-4'>
+            <div class="flex flex-col">
+                <div>{{ __('contacts') }}</div>
+                <div class="">{{ renderPhone($settings->get('phone1')->value) }}</div>
+                <div class="">{{ renderPhone($settings->get('phone2')->value) }}</div>
+                <div class="">{{ $settings->get('address1')->value }}</div>
+            </div>
+            <a href="https://www.instagram.com/lyc.by/" title="Instagram" class='text-xl' target="_blank"><i class="fab fa-instagram"></i></a>
+            <div class="flex flex-col space-y-1">
+                @foreach($pages as $page)
+                    <a href="{{ route('page.index', [$page->slug]) }}" title="{{ $page->name }}" class="">{{ $page->name }}</a>
+                @endforeach
+            </div>
         </div>
-        <div class="flex flex-col space-y-1">
-            @foreach($pages as $page)
-                <a href="{{ route('page.index', [$page->slug]) }}" title="{{ $page->name }}" class="">{{ $page->name }}</a>
-            @endforeach
-        </div>
+        
         <img src="{{ Storage::url('payments_min.jpg') }}" class="h-60" alt="">
     </div>
 </footer>
